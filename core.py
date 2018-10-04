00b697fe5fcfac58a4434576eb16c252a60e3e5f
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-03 21:31:10 -0500

    Added in New Classes
    I added the Trip, Mechanic, Trip Coordinator
    , and Driver classes. These all have there
    special methods which come together in
    the trip class. I created the main method
    in Trip that runs dependent to each
    prepare's class. It still needs some error
    fixes, but the structure of it is their.

--- core.py
+++ core.py
@@ -24,3 +24,52 @@ class Wheel(Gear):
 
     def diameter(self):
         return self.rim + (self.tire * 2)
+
+
+class Trip:
+    def __init__(self, bicycles, customers, vehicle):
+        self.bicycles = bicycles
+        self.customers = customers
+        self.vehicle = vehicle
+
+    def prepare(self,preparers):
+        for preparer in preparers:
+            if isinstance(preparer, Mechanic):
+                preparer.prepare_bicycles(self.bicycles)
+            else if isinstance(preparer,TripCoordinator):
+                preparer.buy_food(self.customers)
+            else if isinstance(preparer,Driver):
+                preparer.gas_up(self.vehicle)
+                preparer.fill_water_tank(self.vehicle)
+
+class Mechanic:
+    def __init__(self, bicycles):
+        self.bicycles = bicycles
+
+    def prepare_bicycles(self):
+        fixed_bikes = []
+        for bike in self.bicycles:
+            fixed_bikes.append(self.prepare_bicycle(bike))
+        return fixed_bikes
+
+    def prepare_bicycle(self):
+        return None
+
+
+class TripCoordinator:
+    def __init__(self, customers):
+        self.customers = customers
+
+    def buy_food(self):
+        return None
+
+
+class Driver:
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
+
+    def gas_up(self):
+        return None
+    
+    def fill_water_tank(self):
+        return None
